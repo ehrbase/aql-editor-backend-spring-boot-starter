919bbac066e95ee62a62bb59bf3085c45feb398a
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2021-03-04 22:50:14 +0200

    feat(NUM-1136): DefaultRestClient with optional TemplateProvider

--- src/main/java/org/ehrbase/aqleditor/configuration/EhrConfig.java
+++ src/main/java/org/ehrbase/aqleditor/configuration/EhrConfig.java
@@ -7,10 +7,7 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.ehrbase.aqleditor.properties.EhrProperties;
-import org.ehrbase.aqleditor.service.ehrbase.FakeEhrTemplateProvider;
 import org.ehrbase.client.openehrclient.OpenEhrClientConfig;
 import org.ehrbase.client.openehrclient.defaultrestclient.DefaultRestClient;
 import org.springframework.context.annotation.Bean;
@@ -30,12 +27,7 @@ public class EhrConfig {
         new UsernamePasswordCredentials(
             ehrProperties.getUsername(), ehrProperties.getPassword()));
 
-    CloseableHttpClient httpClient =
-        HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
-
     return new DefaultRestClient(
-        new OpenEhrClientConfig(new URI(ehrProperties.getRestApiUrl())),
-        new FakeEhrTemplateProvider(),
-        httpClient);
+        new OpenEhrClientConfig(new URI(ehrProperties.getRestApiUrl())));
   }
 }
